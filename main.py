8153b5b4a52a6ba4f1a8f18358340b018536ac03
Annamalaii45 <100117262+Annamalaii45@users.noreply.github.com>
2022-02-22 22:52:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+company_name="""                                     Walmart"""
+purpose="""                              Job Application For Sales Manager"""
+
+
+name=input("Name: ")
+education_qualification=input("Education Qualification ")
+father_name=input("Father's Name ")
+hobby=input("Hobby ")
+weight=input("Weight ")
+kilogram=int(weight)*1
+birth_year=input("Birth Year ")
+age=2022-int(birth_year)
+job_experience=("Job Experience ")
+certificate=input("certificate")
+address=input("Address")
+
+
+print(company_name)
+print(purpose)
+print(name)
+print(age)
+print(birth_year)
+print(weight)
+print(kilogram)
+print(address)
+print(education_qualification)
+print(father_name)
+print(hobby)
+print(job_experience)
+print(certificate)
+
